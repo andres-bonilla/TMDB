62f50add5bc631e2f66f0ee33e7bbeb77eb2dad3
andres-bonilla <x.andresbonilla@gmail.com>
2023-08-04 22:07:14 -0300

    Estilo base para footbar agregado

--- src/components/Footbar.jsx
+++ src/components/Footbar.jsx
@@ -1,14 +1,13 @@
 import tmdbLogo from "../essets/tmdb_logo.svg";
+import "../styles/footbar.css";
 
 export const Footbar = () => {
   return (
-    <div>
-      <label>
-        Powered by:
-        <a href="https://www.themoviedb.org">
-          <img src={tmdbLogo} alt="TMDB" width={"125px"} />
-        </a>
-      </label>
+    <div id="footbar">
+      <label for="tmdb Logo">Powered by:</label>
+      <a href="https://www.themoviedb.org" id="tmdb Logo">
+        <img src={tmdbLogo} alt="TMDB" width={"170px"} />
+      </a>
     </div>
   );
 };
