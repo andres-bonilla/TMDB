a323633d7af63d4a33d519865e2edfdd0a19fe3f
andres-bonilla <x.andresbonilla@gmail.com>
2023-08-04 22:07:14 -0300

    Estilo y grid aplicado para los resultados de busqueda

--- src/commons/Card.jsx
+++ src/commons/Card.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import { urlImg } from "../utils/utils";
 import { useSelector } from "react-redux";
+import "../styles/results.css";
 
 export const Card = ({ data }) => {
   const urlBaseImg = useSelector((state) => state.img);
 
   return (
-    <div key={data.id}>
+    <div className="cardBox" key={data.id}>
       <Link to={`/${data["media_type"]}/${data.id}`}>
         <img
+          className="cardImg"
           src={urlImg(urlBaseImg, data["poster_path"] || data["profile_path"])}
           alt={`${data.name || data["original_title"]}`}
         />
-
-        <h3>{data.name || data["original_title"]}</h3>
+        <div className="cardTitleBox">
+          <h3 className="cardTitle">{data.name || data["original_title"]}</h3>
+        </div>
       </Link>
     </div>
   );
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "../commons/Card";
 import { useSelector } from "react-redux";
+import "../styles/results.css";
 
 export const Results = () => {
   const busqueda = useSelector((state) => state.result);
   return (
-    <div>
+    <div id="resultsBox">
       {busqueda.map((resultado) => {
         return <Card data={resultado} key={resultado.id} />;
       })}
