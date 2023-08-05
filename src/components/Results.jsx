5510a4b5c6539f7aa81e2a85f8f811ffcba42231
andres-bonilla <x.andresbonilla@gmail.com>
2023-08-04 22:07:14 -0300

    Contador de elementos por pagina segun tamaño del Grid agregado

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,43 @@
 import "../styles/results.css";
 import React, { useEffect, useState } from "react";
 import { Card } from "../commons/Card";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CheckTabs } from "./CheckTabs";
 import { Link } from "react-router-dom";
+import { setMaxElementsGrid } from "../store/searchSlice";
 
 export const Results = () => {
+  const dispatch = useDispatch();
+
+  const [columnsNumber, setColumnsNumber] = useState(0);
+
   const search = useSelector((state) => state.search);
+
+  useEffect(() => {
+    const handleColumnsNumber = () => {
+      const grid = document.getElementById("resultsGrid");
+      const gridStyle = document.defaultView.getComputedStyle(grid);
+
+      setColumnsNumber(
+        gridStyle.getPropertyValue("grid-template-columns").split(" ").length
+      );
+    };
+
+    window.addEventListener("resize", handleColumnsNumber);
+
+    return () => {
+      window.removeEventListener("resize", handleColumnsNumber);
+    };
+  }, []);
+
+  useEffect(() => {
+    const grid = document.getElementById("resultsGrid");
+    const gridStyle = document.defaultView.getComputedStyle(grid);
+    const columnCount = gridStyle
+      .getPropertyValue("grid-template-columns")
+      .split(" ").length;
+    dispatch(setMaxElementsGrid(columnCount * 6));
+  }, [columnsNumber]);
 
   // const [numColumns, setNumColumns] = useState("1");
   // useEffect(() => {
@@ -32,7 +63,7 @@ export const Results = () => {
     <div id="resultsBox">
       <CheckTabs />
       <div id="resultsGrid">
-        {search.data.map((resultado) => {
+        {search.actualPage.map((resultado) => {
           return <Card data={resultado} key={resultado.id} />;
         })}
       </div>
--- src/store/searchSlice.js
+++ src/store/searchSlice.js
@@ -1,10 +1,83 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   data: [],
+//   status: "idle",
+// };
+
+// export const getResult = createAsyncThunk("search/get", async (params) => {
+//   const { mediaType, words, oldSearch } = params;
+//   const { data } = await axios.get(
+//     `/api/search/${mediaType}?by_words=${words}`,
+//     {
+//       signal: oldSearch ? oldSearch.signal : null,
+//     }
+//   );
+//   return data;
+// });
+
+// export const searchSlice = createSlice({
+//   name: "search",
+//   initialState,
+//   reducers: {
+//     loadResult(state, action) {
+//       return state;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getResult.pending, (state, action) => {
+//         state.status = "loading";
+//       })
+//       .addCase(getResult.fulfilled, (state, action) => {
+//         state.data = action.payload;
+//         state.status = "idle";
+//       })
+//       .addCase(getResult.rejected, (state, action) => {
+//         state.status = "error";
+//       });
+//   },
+// });
+
+// export const { loadResult } = searchSlice.actions;
+
+// export default searchSlice.reducer;
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  data: [],
+  actualPage: [],
+  spareData: [],
+  maxElementsGrid: 20,
+  lastReqPageTMDB: 1,
+  lastPageTMDB: 0,
   status: "idle",
 };
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setMaxElementsGrid(state, action) {
+      state.maxElementsGrid = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getResult.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getResult.fulfilled, (state, action) => {
+        state.actualPage = action.payload;
+        state.status = "idle";
+      })
+      .addCase(getResult.rejected, (state, action) => {
+        state.status = "error";
+      });
+  },
+});
 
 export const getResult = createAsyncThunk("search/get", async (params) => {
   const { mediaType, words, oldSearch } = params;
@@ -17,29 +90,6 @@ export const getResult = createAsyncThunk("search/get", async (params) => {
   return data;
 });
 
-export const searchSlice = createSlice({
-  name: "search",
-  initialState,
-  reducers: {
-    loadResult(state, action) {
-      return state;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(getResult.pending, (state, action) => {
-        state.status = "loading";
-      })
-      .addCase(getResult.fulfilled, (state, action) => {
-        state.data = action.payload;
-        state.status = "idle";
-      })
-      .addCase(getResult.rejected, (state, action) => {
-        state.status = "error";
-      });
-  },
-});
-
-export const { loadResult } = searchSlice.actions;
+export const { setMaxElementsGrid } = searchSlice.actions;
 
 export default searchSlice.reducer;
